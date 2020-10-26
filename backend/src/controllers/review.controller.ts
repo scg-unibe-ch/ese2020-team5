65471fb35597a340808994730db60f7a1c23ff2f
Dominique Roux <dominique.roux4@gmail.com>
2020-10-26 15:49:34 +0100

    Updated the review controller in such a way that the reviews can only be updated by the initial user and only be deleted by the initial user or an admin.

--- backend/src/controllers/review.controller.ts
+++ backend/src/controllers/review.controller.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { Router, Request, Response } from 'express';
 import { Review } from '../models/review.model';
 import {getUserId, verifyToken} from '../middlewares/checkAuth';
+import {User} from '../models/user.model';
 
 const reviewController: Router = express.Router();
 
@@ -16,6 +17,9 @@ reviewController.put('/:id', verifyToken, (req: Request, res: Response) => {
     Review.findByPk(req.params.id)
         .then(found => {
             if (found != null) {
+                if (found.userId !== getUserId(req)) {
+                    return Promise.reject('You are not authorized to do this!');
+                }
                 found.update(req.body).then(updated => {
                     res.status(200).send(updated);
                 });
@@ -28,10 +32,18 @@ reviewController.put('/:id', verifyToken, (req: Request, res: Response) => {
 });
 
 reviewController.delete('/:id', verifyToken, (req: Request, res: Response) => {
+    const userId = getUserId(req);
     Review.findByPk(req.params.id)
         .then(found => {
             if (found != null) {
-                found.destroy().then(() => res.status(200).send(found));
+                return User.findByPk(userId)
+                    .then(user => {
+                        if (user.isAdmin !== 1 && found.userId !== userId) {
+                            return Promise.reject('You are not authorized to do this!');
+                        } else {
+                            return found.destroy().then(() => res.status(200).send(found));
+                        }
+                    });
             } else {
                 res.sendStatus(404);
             }
