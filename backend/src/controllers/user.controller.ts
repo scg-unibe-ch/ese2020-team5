37003899fbbad0cbde5f41f92f9b1f4597a4a5b3
Joseph Schnabel <joseph.schnabel@students.unibe.ch>
2020-10-27 11:05:23 +0100

    merged changes into my branch

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,10 +1,12 @@
 
 import express, { Router, Request, Response } from 'express';
-import { UserService } from '../services/user.service';
-import { verifyToken } from '../middlewares/checkAuth';
+import { UserService, AdminService } from '../services/user.service';
+import {getUserId, verifyToken} from '../middlewares/checkAuth';
+import {isNumber} from 'util';
 
 const userController: Router = express.Router();
 const userService = new UserService();
+const adminService = new AdminService();
 
 userController.post('/register',
     (req: Request, res: Response) => {
@@ -18,9 +20,23 @@ userController.post('/login',
     }
 );
 
+userController.get('/all', verifyToken, // you can add middleware on specific requests like that
+    (req: Request, res: Response) => {
+        userService.getAll(getUserId(req)).then(users => res.send(users)).catch(err => res.status(500).send(err));
+    }
+);
+
 userController.get('/', verifyToken, // you can add middleware on specific requests like that
     (req: Request, res: Response) => {
-        userService.getAll().then(users => res.send(users)).catch(err => res.status(500).send(err));
+        userService.getUser(getUserId(req)).then(usr => res.send(usr)).catch(err => res.status(500).send(err));
+    }
+);
+
+userController.delete('/:id', verifyToken, (req: Request, res: Response) => {
+        const deleterId: number = parseInt(req.params.id, 10);
+        adminService.delete(getUserId(req), deleterId)
+            .then(deleted => res.send(deleted))
+            .catch(err => res.status(500).send(err));
     }
 );
 
