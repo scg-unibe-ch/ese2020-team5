e5d06f0593b5b693deda900751f6282d814a304d
Joseph Schnabel <joseph.schnabel@students.unibe.ch>
2020-11-02 21:58:31 +0100

    Profile (wip)
    Profile doesn't show content. Not sure how to implement.
    
    Search field css difficulties with alignment

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { ActivatedRoute,Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { User } from '../../models/user.model';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -12,15 +15,45 @@ import { Router } from '@angular/router';
 export class ProfileComponent implements OnInit {
   secureEndpointResponse = '';
   userNameOrEmail = '';
+  profileForm: FormGroup;
+  user: User = null;
+  id: number;
 
   constructor(
     private authService: AuthService,
     private httpClient: HttpClient,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
     this.userNameOrEmail = localStorage.getItem('userNameOrEmail');
+    this.route.params.subscribe(params => {
+      this.userService.getLoadedUser().then(user => {
+        this.id = params.id;
+        this.user = (user.filter(entry => entry.userId === parseInt(params.id, 10)))[0];
+        if (this.user) {
+          this.initialize();
+        }
+      });
+    });
+  }
+
+  initialize(): void {
+    this.profileForm = this.formBuilder.group({
+      userId: [this.user.userId, [Validators.required]],
+      userName: [this.user.userName, [Validators.required]],
+      email: [this.user.email, [Validators.required]],
+      firstName: [this.user.firstName, [Validators.required]],
+      lastName: [this.user.lastName, [Validators.required]],
+      gender: [this.user.gender, [Validators.required]],
+      country: [this.user.country, [Validators.required]],
+      city: [this.user.city, [Validators.required]],
+      zipCode: [this.user.zipCode, [Validators.required]],
+      phoneNr: [this.user.phoneNr, [Validators.required]]
+    });
   }
 
   logout(): void {
@@ -35,4 +68,9 @@ export class ProfileComponent implements OnInit {
       this.secureEndpointResponse = 'Unauthorized';
     });
   }
+
+  updateProfile(): void {
+    this.userService.updateProfile(Object.assign({ userId: this.user.userId }, this.profileForm.value))
+  }
+
 }
