30d2f502cac6f3bc7539bc86942c72493299a5be
Kajan Kugathasan <kajan.kugathasan@students.unibe.ch>
2020-11-12 17:38:20 +0100

    Added compact product view in list format

---
+++ frontend/src/app/components/custom/compact-product-list/compact-product-list.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CompactProductListComponent } from './compact-product-list.component';
+
+describe('CompactProductComponent', () => {
+  let component: CompactProductListComponent;
+  let fixture: ComponentFixture<CompactProductListComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CompactProductListComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CompactProductListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
