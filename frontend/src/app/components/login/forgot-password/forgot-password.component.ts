4fa38d7fa606eefd70e055fb027483b853fc4318
Thirith Yang <thirith.yang@students.unibe.ch>
2020-11-18 11:21:27 +0100

    Please take a look, and correct.

--- frontend/src/app/components/login/forgot-password/forgot-password.component.ts
+++ frontend/src/app/components/login/forgot-password/forgot-password.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent implements OnInit {
+  hide: boolean;
 
   constructor() { }
 
