026563697fbcf9276540aa51e558be570571156b
Kajan Kugathasan <kajan.kugathasan@students.unibe.ch>
2020-11-23 00:37:57 +0100

    Added rating to review model

--- frontend/src/app/models/review.model.ts
+++ frontend/src/app/models/review.model.ts
@@ -2,6 +2,7 @@ export class Review {
   constructor(
     public reviewId: number,
     public review: string,
+    public rating: number,
     public productId: number,
     public userId: number
   ) {}
