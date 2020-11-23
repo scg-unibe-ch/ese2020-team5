3f0aa89907879050b11cd8678e04be8184cb7d6a
Thirith Yang <thirith.yang@students.unibe.ch>
2020-11-23 21:55:00 +0100

    It's the first test, please check it and correct it if necessary

--- frontend/src/app/components/product/product.component.spec.ts
+++ frontend/src/app/components/product/product.component.spec.ts
@@ -22,4 +22,21 @@ describe('ProductComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should add items to "data"', () => {
+    expect(component.data.length).toBe(1); // since you have initialized the variable
+    component.productName = "Prod1";
+    component.quantity = 1;
+    component.addItem();  // this will trigger the method
+    expect(component.data.length).toBe(4); // this will show that the entry was added in "this.data"
+  });
+  describe('when a user deletes a product', () => {
+    beforeEach(() => {
+      getProducts()[0].componentInstance.productDelete.emit();
+    });
+  });
+  it('should delete the product', () => {
+    expect(dependencies.productService.deleteProduct).toHaveBeenCalledWith(
+      'product'
+    );
+  });
 });
--- frontend/src/app/services/user.service.spec.ts
+++ frontend/src/app/services/user.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
+import { of } from 'rxjs';
+import {all} from "codelyzer/util/function";
 
 describe('UserService', () => {
   let service: UserService;
@@ -12,5 +14,35 @@ describe('UserService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+
+    // Add tests for all() method
+    describe('all', () => {
+      it('should return a collection of users', () => {
+        const userResponse = [
+          {
+            id: '1',
+            name: 'Admin',
+            role: 'Admin',
+          },
+          {
+            id: '2',
+            name: 'User',
+            role: 'Developer',
+          },
+          {
+            id: '3',
+            name: 'Guest',
+            role: 'Guest',
+          }
+        ];
+        let response;
+        spyOn(UserService, 'all').and.returnValue(of(userResponse));
+
+        UserService.all().subscribe(res => {
+          response = res;
+        });
+        expect(response).toEqual(userResponse);
+      });
+    });
   });
 });
