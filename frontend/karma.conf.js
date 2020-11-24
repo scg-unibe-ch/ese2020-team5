1fd4b0d56670155d8ed44134ddc8749ccfcffc65
KnownInter <55671661+KnownInter@users.noreply.github.com>
2020-11-24 05:21:11 +0100

    Updated karma conf to fix an error

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -33,6 +33,7 @@ module.exports = function (config) {
       }
     },
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    failOnEmptyTestSuite: false
   });
 };
