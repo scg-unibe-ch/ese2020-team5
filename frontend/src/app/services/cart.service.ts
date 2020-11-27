e0880c9f7ad8c36d68a3c2d6872084a776b6c516
schnajos <44681409+schnajos@users.noreply.github.com>
2020-11-27 03:09:22 +0100

    implemented part of cartService

--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -10,6 +10,25 @@ export class CartService {
 
   constructor(
     private httpClient: HttpClient,
-    private cartItem: CartItem
   ) { }
+
+  getCartItems(): Promise<CartItem[]> {
+    return new Promise<CartItem[]>((resolve, reject) => {
+      this.httpClient.get(environment.endpointURL + 'cart/' ).subscribe((cartItems: CartItem[]) => {
+        resolve(cartItems);
+      }, (error: any) => {
+        reject(error);
+      });
+    });
+  }
+
+  deleteCartItem(cartItem: CartItem) {
+    return new Promise<CartItem>((resolve, reject) => {
+      this.httpClient.delete(environment.endpointURL + 'cart/' + cartItem.productId).subscribe((deletedCartItem: CartItem) => {
+        resolve(deletedCartItem);
+      }, (error: any) => {
+        reject(error);
+      });
+    });
+  }
 }
