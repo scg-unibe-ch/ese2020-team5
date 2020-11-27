dfe6a2bc6ba52a7b0a178b3182b3be326000d0f0
schnajos <44681409+schnajos@users.noreply.github.com>
2020-11-27 03:10:55 +0100

    getCartItem & deleteCartItem added to shopping-cart.ts  (not finished)

--- frontend/src/app/components/shopping-cart/shopping-cart.component.ts
+++ frontend/src/app/components/shopping-cart/shopping-cart.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CartService } from '../../services/cart.service';
 import { Product } from '../../models/product.model';
 import { CartItem } from '../../models/cartItem.model';
+import { AuthService } from '../../services/auth.service';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -11,15 +14,66 @@ import { CartItem } from '../../models/cartItem.model';
 })
 export class ShoppingCartComponent implements OnInit {
 
-  cartItems: Product[];
+  cartItems: CartItem[];
+  buyerId: number;
+  productId: number;
+  cartItem: CartItem;
+  product: Product;
+  amountOrTime: number;
 
   constructor(
     private cartService: CartService,
     private productService: ProductService,
-    private cartItem: CartItem
+    private authService: AuthService,
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
+    this.userService.getUser().then(user => {
+      this.buyerId = user.userId;
+      this.getCartItems();
+    });
+  } 
+
+  getCartItems(): void {
+    if (this.authService.isLoggedIn()) {
+      this.userService.getUser().then(user => {
+        this.cartService.getCartItems().then(cartItems => {
+          this.cartItems = cartItems;
+          for (let cartItem of cartItems) {
+            console.log(cartItem.productId)
+            this.productService.getProductById(cartItem.productId).then(product => {
+              this.product = product;
+              console.log(product.title, product.productId, product.price)
+            });
+          }
+        });
+      });
+    } else {
+      location.assign('login?returnURL=' + this.router.url);
+    }
   }
 
+  
+  deleteCartItem (): void {
+    if (this.authService.isLoggedIn() && this.product.approved) {
+      this.userService.getUser().then(user => {
+        const cartItem: CartItem = {
+          buyerId: user.userId,
+          productId: this.product.productId,
+          amountOrTime: 1
+        }
+        this.cartService.deleteCartItem(cartItem);
+        location.reload();
+      });
+    } else {
+      location.assign('login?returnURL=' + this.router.url);
+    }
+  }
+
+  incrementAmountOrTime() {
+    
+  }
 }
