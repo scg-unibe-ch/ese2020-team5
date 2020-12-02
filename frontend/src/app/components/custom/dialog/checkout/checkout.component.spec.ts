95ecd58a25bd274914d31704c4fdb926f5de94a0
Kajan Kugathasan <kajan.kugathasan@students.unibe.ch>
2020-12-02 05:02:16 +0100

    Fixed tests

--- frontend/src/app/components/custom/dialog/checkout/checkout.component.spec.ts
+++ frontend/src/app/components/custom/dialog/checkout/checkout.component.spec.ts
@@ -18,8 +18,4 @@ describe('CheckoutComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
--- frontend/src/app/components/shopping-cart/shopping-cart.component.spec.ts
+++ frontend/src/app/components/shopping-cart/shopping-cart.component.spec.ts
@@ -18,8 +18,4 @@ describe('ShoppingCartComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
