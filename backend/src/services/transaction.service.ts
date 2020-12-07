7ed1ee6cfcd22ee08a7b88765a1180403cfbade8
Dominique Roux <dominique.roux@protonmail.com>
2020-12-07 17:10:21 +0100

    Updated the transaction service: If the product is a service or only for lent, do not decrement the amount or change the status.

--- backend/src/services/transaction.service.ts
+++ backend/src/services/transaction.service.ts
@@ -57,10 +57,8 @@ export class TransactionService {
                         // Update the product (e.g. set as unavailable or decrement the stock
                         // Hail to Jethro, who enforced me to create an Interface for this.
                         const productUpdate: ProductUpdate = {};
-                        if (product.type === 1) {
-                            // if it is a service, it is automatically unavailable
-                            productUpdate.status = 1;
-                        } else {
+                        // If the product is a service or only for lent, then do not decrement the amount (or even the status)
+                        if (product.type !== 1 && product.sellOrLend !== 1) {
                             // if the resulting amount of a product is 0, it becomes unavailable
                             if ( product.amount - transaction.amountOrTime === 0) {
                                 productUpdate.status = 1;
