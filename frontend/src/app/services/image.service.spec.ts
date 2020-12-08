0ae83b3287a65c9ba61362d37e4efc92cf0e00a7
Kajan Kugathasan <kajan.kugathasan@students.unibe.ch>
2020-12-08 22:22:43 +0100

    Added few tests

--- frontend/src/app/services/image.service.spec.ts
+++ frontend/src/app/services/image.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ImageService } from './image.service';
+import { environment } from '../../environments/environment';
 
 describe('ImageService', () => {
   let service: ImageService;
@@ -8,5 +8,18 @@ describe('ImageService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(ImageService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should return the source url of the image', () => {
+    expect(service.getImageUrl('testFile.png')).toBe(environment.endpointURL + 'images/' + 'testFile.png');
+  });
+
+  it('should return the original source url', () => {
+    const url = 'https://testing.test/test.png';
+    expect(service.getImageUrl(url)).toBe(url);
   });
 });
--- frontend/src/app/services/review.service.spec.ts
+++ frontend/src/app/services/review.service.spec.ts
@@ -1,12 +1,46 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ReviewService } from './review.service';
+import { Review } from '../models/review.model';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ReviewService', () => {
   let service: ReviewService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(ReviewService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should calculate the average rating', () => {
+    const dummyReviews: Review[] = [
+      {
+        reviewId: 1,
+        review: 'Test1234',
+        rating: 1,
+        productId: 2,
+        userId: 4
+      },
+      {
+        reviewId: 2,
+        review: 'Pretty good',
+        rating: 4,
+        productId: 2,
+        userId: 1
+      },
+      {
+        reviewId: 3,
+        review: 'It\'s ok',
+        rating: 2.5,
+        productId: 2,
+        userId: 3
+      }
+    ];
+    expect(service.getAvgRating(dummyReviews)).toBe(2.5);
   });
 });
